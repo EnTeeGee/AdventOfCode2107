78f92b2fd78b6ebab1122dc055a879e744a77d59
EnTeeGee <nickdyke2@gmail.com>
2017-12-02 14:21:09 +0000

    Added first puzzle of the second day.

---
+++ Solutions/Day02Problem1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    public class Day02Problem1
+    {
+        public string GenerateAnswer(string input)
+        {
+            var rows = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return rows.Select(it => this.RowToDiff(it)).Sum().ToString();
+        }
+
+        private int RowToDiff(string row)
+        {
+            var numbers = row.Split((char[])null, StringSplitOptions.RemoveEmptyEntries).Select(it => Convert.ToInt32(it));
+            return numbers.Max() - numbers.Min();
+        }
+    }
+}
