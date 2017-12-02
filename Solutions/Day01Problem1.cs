73a23f9eba2f762938524070bbd2723c891e2565
EnTeeGee <nickdyke2@gmail.com>
2017-12-02 13:37:19 +0000

    Initial commit of files from the first day.

---
+++ Solutions/Day01Problem1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    public class Day01Problem1
+    {
+        public string GenerateAnswer(string input)
+        {
+            var numberList = input.Select(it => Convert.ToInt32(it.ToString())).ToList();
+            var offsetList = numberList.Concat(new List<int> { numberList[0] }).Skip(1).ToList();
+
+            return numberList.Zip(offsetList, (a, b) => new { a, b }).Where(it => it.a == it.b).Sum(it => it.a).ToString();
+        }
+    }
+}
---
+++ Solutions/Day01Problem2.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    public class Day01Problem2
+    {
+        public string GenerateAnswer(string input)
+        {
+            var midPoint = input.Length / 2;
+            var numberList = input.Select(it => Convert.ToInt32(it.ToString())).ToList();
+            var offsetList = numberList.Concat(numberList.Take(midPoint)).Skip(midPoint).ToList();
+
+            return numberList.Zip(offsetList, (a, b) => new { a, b }).Where(it => it.a == it.b).Sum(it => it.a).ToString();
+        }
+    }
+}
