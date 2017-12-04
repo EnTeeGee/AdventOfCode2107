78d1dff2cb28a1c4e194521da9f6a9f044c2e7f1
EnTeeGee <nickdyke2@gmail.com>
2017-12-04 22:48:04 +0000

    Solutions for day 4 puzzles.

---
+++ Solutions/Day04Problem2.cs
@@ -0,0 +1,35 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day04Problem2 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return lines.Select(it => this.IsPassPhraseValid(it)).Count(it => it).ToString();
+        }
+
+        private bool IsPassPhraseValid(string passPhrase)
+        {
+            var words = passPhrase.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+            words = words.Select(it => new String(it.OrderBy(letter => letter).ToArray())).ToArray();
+
+            for (var i = 0; i < words.Length; i++)
+            {
+                for (var j = i + 1; j < words.Length; j++)
+                {
+                    if (words[i] == words[j])
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
