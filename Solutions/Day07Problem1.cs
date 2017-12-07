a88c2f5bf98cf070cc81b980bebc70f9e4e4584c
EnTeeGee <nickdyke2@gmail.com>
2017-12-07 21:07:28 +0000

    Solutions for day 7.

---
+++ Solutions/Day07Problem1.cs
@@ -0,0 +1,45 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day07Problem1 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var progs = lines.Select(it => new Prog(it)).Where(it => it.Nodes.Any()).ToList();
+
+            var currentHead = progs[0];
+
+            while (true)
+            {
+                var parent = progs.FirstOrDefault(it => it.Nodes.Contains(currentHead.Name));
+                if(parent == null)
+                    return currentHead.Name;
+
+                currentHead = parent;
+            }
+        }
+
+        private class Prog
+        {
+            public Prog(string input)
+            {
+                var items = input.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                this.Name = items[0];
+
+                if(items.Length > 2)
+                    this.Nodes = items.Skip(3).Select(it => it.TrimEnd(',')).ToList();
+                else
+                    this.Nodes = new List<string>();
+            }
+
+            public string Name { get; }
+
+            public List<string> Nodes { get; }
+        }
+    }
+}
---
+++ Solutions/Day07Problem2.cs
@@ -0,0 +1,105 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day07Problem2 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var progs = lines.Select(it => new Prog(it)).ToList();
+
+            foreach(var item in progs)
+                item.AssignNodes(progs);
+
+            var headNode = this.GetHead(progs.Where(it => it.Nodes.Any()).ToList());
+
+            return headNode.GetRequiredWeight().ToString();
+        }
+
+        private Prog GetHead(List<Prog> progs)
+        {
+            var currentHead = progs[0];
+
+            while (true)
+            {
+                var parent = progs.FirstOrDefault(it => it.NodeNames.Contains(currentHead.Name));
+                if (parent == null)
+                    return currentHead;
+
+                currentHead = parent;
+            }
+        }
+
+        private class Prog
+        {
+            private int? totalWeight;
+
+            public Prog(string input)
+            {
+                var items = input.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                this.Name = items[0];
+                this.Weight = Convert.ToInt32(items[1].Trim('(', ')'));
+                this.Nodes = new List<Prog>();
+
+                if (items.Length > 2)
+                    this.NodeNames = items.Skip(3).Select(it => it.TrimEnd(',')).ToList();
+                else
+                {
+                    this.NodeNames = new List<string>();
+                    this.totalWeight = this.Weight;
+                }
+            }
+
+            public void AssignNodes(List<Prog> nodeList)
+            {
+                this.Nodes.AddRange(this.NodeNames.Join(nodeList, it => it, it => it.Name, (a, b) => b));
+            }
+
+            public int GetTotalWeight()
+            {
+                if (this.totalWeight == null)
+                    this.totalWeight = this.Nodes.Sum(it => it.GetTotalWeight()) + this.Weight;
+
+                return this.totalWeight.Value;
+            }
+
+            public int GetRequiredWeight()
+            {
+                var unbalancedNode = this.Nodes.FirstOrDefault(it => this.Nodes.Count(node => node.GetTotalWeight() == it.GetTotalWeight()) == 1);
+                var balancedNode = this.Nodes.First(it => it != unbalancedNode);
+
+
+                return unbalancedNode.GetRequiredWeight(balancedNode.GetTotalWeight());
+            }
+
+            public int GetRequiredWeight(int expectedWeightOfStack)
+            {
+                var unbalancedNode = this.Nodes.FirstOrDefault(it => this.Nodes.Count(node => node.GetTotalWeight() == it.GetTotalWeight()) == 1);
+
+                if(unbalancedNode == null)
+                {
+                    var childWeight = this.Nodes[0].GetTotalWeight();
+                    return expectedWeightOfStack - (childWeight * this.Nodes.Count);
+                }
+
+                var balancedNode = this.Nodes.First(it => it != unbalancedNode);
+
+                return unbalancedNode.GetRequiredWeight(balancedNode.GetTotalWeight());
+            }
+
+            public string Name { get; }
+
+            public int Weight { get; }
+
+            public List<string> NodeNames { get; }
+
+            public List<Prog> Nodes { get; }
+        }
+    }
+}
