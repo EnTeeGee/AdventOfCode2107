6ef7786c5886f67248fdf5244d894b99aa49f0a6
EnTeeGee <nickdyke2@gmail.com>
2017-12-10 21:24:39 +0000

    Solutions for ay 10.

---
+++ Solutions/Day10Problem1.cs
@@ -0,0 +1,39 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day10Problem1 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var values = Enumerable.Range(0, 256).ToArray();
+            var currentPosition = 0;
+            var skipSize = 0;
+
+            var lengths = input.Split(',').Select(it => Convert.ToInt32(it)).ToList();
+
+            foreach(var length in lengths)
+            {
+                var subset = values.Concat(values).Skip(currentPosition).Take(length).ToList();
+                subset.Reverse();
+                this.ApplyValuesToArray(values, subset, currentPosition);
+                currentPosition = (currentPosition + length + skipSize) % values.Length;
+                ++skipSize;
+            }
+
+            return (values[0] * values[1]).ToString();
+        }
+
+        private void ApplyValuesToArray(int[] values, List<int> updatedValues, int position)
+        {
+            for(var i = 0; i < updatedValues.Count; i++)
+            {
+                var index = (i + position) % values.Length;
+                values[index] = updatedValues[i];
+            }
+        }
+    }
+}
---
+++ Solutions/Day10Problem2.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day10Problem2 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var values = Enumerable.Range(0, 256).Select(it => (byte)it).ToArray();
+            var currentPosition = 0;
+            var skipSize = 0;
+
+            var lengths = input
+                .Trim()
+                .Select(it => Convert.ToByte(it))
+                .Concat(new List<byte> { 17, 31, 73, 47, 23 })
+                .ToList();
+
+            for (var i = 0; i < 64; i++)
+            {
+                foreach (var length in lengths)
+                {
+                    var subset = values.Concat(values).Skip(currentPosition).Take(length).ToList();
+                    subset.Reverse();
+                    this.ApplyValuesToArray(values, subset, currentPosition);
+                    currentPosition = (currentPosition + length + skipSize) % values.Length;
+                    ++skipSize;
+                }
+            }
+
+            var blocks = Enumerable.Range(0, 16).Select(it => values.Skip(it * 16).Take(16).ToList()).ToList();
+            var bytes = blocks.Select(it => this.XorCombineList(it)).Select(it => it.ToString("x2"));
+            return string.Join(string.Empty, bytes);
+        }
+
+        private void ApplyValuesToArray(byte[] values, List<byte> updatedValues, int position)
+        {
+            for (var i = 0; i < updatedValues.Count; i++)
+            {
+                var index = (i + position) % values.Length;
+                values[index] = updatedValues[i];
+            }
+        }
+
+        private byte XorCombineList(List<byte> input)
+        {
+            return input.Aggregate((a, b) => (byte)(a ^ b));
+        }
+    }
+}
