c2c21d2a3cad58d8a33c77140bd3a0205be8ca96
EnTeeGee <nickdyke2@gmail.com>
2017-12-13 21:51:12 +0000

    Solutions for day 13.

---
+++ Solutions/Day13Problem1.cs
@@ -0,0 +1,40 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Linq;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day13Problem1 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var walls = lines.Select(it => new Wall(it)).ToList();
+
+            var hits = walls.Where(it => it.IsHit()).Select(it => it.Depth * it.Range).Sum();
+
+            return hits.ToString();
+        }
+
+        private class Wall
+        {
+            public Wall(string input)
+            {
+                var items = input.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                Depth = Convert.ToInt32(items[0].Trim(':'));
+                Range = Convert.ToInt32(items[1]);
+            }
+
+            public int Depth { get; }
+
+            public int Range { get; }
+
+            public bool IsHit()
+            {
+                var cycle = (this.Range - 1) * 2;
+
+                return this.Depth % cycle == 0;
+            }
+        }
+    }
+}
---
+++ Solutions/Day13Problem2.cs
@@ -0,0 +1,53 @@
+﻿using AdventOfCode2017.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2017.Solutions
+{
+    class Day13Problem2 : ISolution
+    {
+        public string GenerateAnswer(string input)
+        {
+            var lines = input.Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+            var walls = lines.Select(it => new Wall(it)).ToList();
+
+            var hasHits = walls.Any(it => it.IsHit());
+            var currentDelay = 0;
+
+            while(hasHits)
+            {
+                foreach (var item in walls)
+                    item.Depth++;
+                ++currentDelay;
+
+                hasHits = walls.Any(it => it.IsHit());
+            }
+
+            return currentDelay.ToString();
+        }
+
+        private class Wall
+        {
+            public Wall(string input)
+            {
+                var items = input.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                Depth = Convert.ToInt32(items[0].Trim(':'));
+                Range = Convert.ToInt32(items[1]);
+            }
+
+            public int Depth { get; set; }
+
+            public int Range { get; }
+
+            public bool IsHit()
+            {
+                var cycle = (this.Range - 1) * 2;
+
+                return this.Depth % cycle == 0;
+            }
+        }
+    }
+}
